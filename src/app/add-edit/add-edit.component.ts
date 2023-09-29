9983802771716098e7efd99092c81393dcbb64b3
Shpetim <shpetimmaliu@gmail.com>
2023-09-30 01:36:20 +0200

    Use form for empForm

--- src/app/add-edit/add-edit.component.ts
+++ src/app/add-edit/add-edit.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit',
   templateUrl: './add-edit.component.html',
   styleUrls: ['./add-edit.component.scss'],
 })
-export class AddEditComponent {}
+export class AddEditComponent {
+  empForm: FormGroup;
+
+  education: string[] = [
+    'Science Engineering',
+    'IT Engineering',
+    'Fishing',
+    'Intermediate',
+  ];
+
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      name: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      university: '',
+      experience: '',
+      company: '',
+    });
+  }
+  onFormSubmit() {
+    if (this.empForm.value) {
+      console.log(this.empForm.value);
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddEditComponent } from './add-edit/add-edit.component';
@@ -28,6 +31,9 @@ import { AppComponent } from './app.component';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatChipsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
