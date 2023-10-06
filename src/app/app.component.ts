4370c6eb36e3748300b5ef556c77781a2ed445b5
Shpetim <shpetimmaliu@gmail.com>
2023-10-06 14:32:19 +0200

    Start add deploy Employee List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddEditComponent } from './add-edit/add-edit.component';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud';
 
-  constructor(private _dialog: MatDialog) {}
+  constructor(
+    private _dialog: MatDialog,
+    private _empService: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
 
   openAddEditEmpForm() {
     this._dialog.open(AddEditComponent);
+  }
+
+  getEmployeeList() {
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
